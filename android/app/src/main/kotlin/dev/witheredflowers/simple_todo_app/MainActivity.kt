4d46b0df139c99ad3e8e137a6ba702c9fb5486a8
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-05-07 18:34:53 +0700

    feat: initial commit

---
+++ android/app/src/main/kotlin/dev/witheredflowers/simple_todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.witheredflowers.simple_todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:simple_todo_app/pages/todos_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Todo App",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TodosPage(),
+    );
+  }
+}
