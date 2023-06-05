48cfc39333743ac851de77f81e72bce027005e65
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-06-05 12:59:29 +0700

    fix: unused import

--- lib/cubits/filtered_todos/filtered_todos_cubit.dart
+++ lib/cubits/filtered_todos/filtered_todos_cubit.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
+// import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:simple_todo_app/cubits/todo_filter/todo_filter_cubit.dart';
-import 'package:simple_todo_app/cubits/todo_list/todo_list_cubit.dart';
-import 'package:simple_todo_app/cubits/todo_search/todo_search_cubit.dart';
+// import 'package:simple_todo_app/cubits/todo_filter/todo_filter_cubit.dart';
+// import 'package:simple_todo_app/cubits/todo_list/todo_list_cubit.dart';
+// import 'package:simple_todo_app/cubits/todo_search/todo_search_cubit.dart';
 import 'package:simple_todo_app/models/todo_model.dart';
 
 part 'filtered_todos_state.dart';
